13a682d7a93366c944b8d03d39a99b4642850575
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-24 18:15:45 -0500

    Initial project created.

---
+++ src/java_class_prg_00_hw_01_card_guessing_game/Hand.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_hw_01_card_guessing_game;
+
+/**
+ *
+ * @author bluebackdev
+ */
+public class Hand {
+    
+}
---
+++ src/java_class_prg_00_hw_01_card_guessing_game/JAVA_CLASS_PRG_00_HW_01_Card_Guessing_Game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_hw_01_card_guessing_game;
+
+/**
+ *
+ * @author bluebackdev
+ */
+public class JAVA_CLASS_PRG_00_HW_01_Card_Guessing_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
